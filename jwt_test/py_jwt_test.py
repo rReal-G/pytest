38c4f68fedaf7bdea5d7e32b5755e9048647edf9
topg <topg@gmail.com>
2024-02-20 04:33:35 +0700

    py jose test

--- jwt_test/py_jwt_test.py
+++ jwt_test/py_jwt_test.py
@@ -1,7 +1,8 @@
 import datetime
+import json
 from time import sleep
 import jwt 
-
+from jose import jws, jwe, constants
 
 key = 'symmetric secret key'
 algo = 'HS256'
@@ -48,6 +49,8 @@ private_bytes = private_key.private_bytes(
                                encryption_algorithm=serialization.NoEncryption()
                                )
 
+
+
 # cipher = public_key.encrypt(b'top G never sleeps', padding.OAEP(
 #         mgf=padding.MGF1(algorithm=hashes.SHA256()),
 #         algorithm=hashes.SHA256(),
@@ -65,6 +68,8 @@ private_bytes = private_key.private_bytes(
 # )
 # print(plain)
 
+
+
 # encoded_rs256 = jwt.encode(object, private_bytes, 'RS256',
 #                            headers={'kid': 'lmao3333'})
 # print(encoded_rs256)
@@ -73,19 +78,55 @@ private_bytes = private_key.private_bytes(
 #                            )
 # print(decoded_rs256)
 
-jwt_payload = jwt.encode(
-    {
-        "exp": datetime.datetime.now(tz=datetime.timezone.utc) \
-        + datetime.timedelta(seconds=2),
-        'iss': 'topG_issuer',
-        'aud': 'topG_audience'    
-    },
-    "secret",
-)
-# sleep(3)
-z = jwt.decode(jwt_payload, "secret", 
-               leeway=2, algorithms=["HS256"],
-               audience='topG_audience',
-               issuer='topG_issuer')
-print(z)
 
+
+
+# jwt_payload = jwt.encode(
+#     {
+#         "exp": datetime.datetime.now(tz=datetime.timezone.utc) \
+#         + datetime.timedelta(seconds=2),
+#         'iss': 'topG_issuer',
+#         'aud': 'topG_audience'    
+#     },
+#     "secret",
+# )
+# # sleep(3)
+# z = jwt.decode(jwt_payload, "secret", 
+#                leeway=2, algorithms=["HS256"],
+#                audience='topG_audience',
+#                issuer='topG_issuer')
+# print(z)
+
+
+
+# signed = jws.sign({'a': 'b'}, 'secret', algorithm='HS256')
+# print(signed)
+# decoded = jwt.decode(signed, 'secret', ['HS256'])
+# print(decoded)
+
+
+
+
+with open('aes_192_key.bin', 'rb') as file:
+    aes_192_bin = file.read()
+
+payload = json.dumps({"some_data": "This is confidential"})  
+jwe_token = jwe.encrypt(
+    payload,
+    #public_bytes,  # Recipient's public key for encryption
+    aes_192_bin, # key wrap with same symmetrix key
+    encryption=constants.ALGORITHMS.A128CBC_HS256, # enc can be whatever
+    algorithm=constants.ALGORITHMS.A192KW, #alg must follow the key
+    kid='lmaohehe'
+)
+jwe_string = jwe_token.decode()
+print(jwe_string)
+
+decrypted_payload = jwe.decrypt(
+    jwe_string, 
+    # private_bytes
+    aes_192_bin # key wrap with same symmetrix key
+    )
+if decrypted_payload:
+    data = json.loads(decrypted_payload.decode()) 
+    print("Decrypted Payload:", data) 
